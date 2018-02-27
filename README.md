9f287585d155fdbe2faaa26bf1281142fda717c0
Henry Qin <root@hq6.me>
2018-02-26 22:11:50 -0800

    Add initial commit with basic foreground and background colors

---
+++ README.md
@@ -0,0 +1,11 @@
+# ColorizeLines.py
+
+There is at least one [existing tool](http://pygments.org/) to perform
+automatic syntax highlighting of code and output ANSI escape sequences.
+Unfortunately, to the best of the author's knowledge, there is no existing
+tool for highlighting lines of log files that contain a particular pattern.
+Vim's `match` and `2match` are close, but a little kludgy to use.
+
+The purpose of this tool is to colorize lines matching a particular pattern, to
+make it easier to highlight relevant lines in a log file, when poring over such
+files with your coworker or manager.
---
+++ colorize.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+
+from sys import argv, stdin
+from docopt import docopt
+from collections import defaultdict
+import re
+import codecs
+
+from signal import signal, SIGPIPE, SIG_DFL
+
+colorMap = {x[0].strip() : x[1] for x in [
+("Black            " , "30"),
+("Red              " , "31"),
+("Green            " , "32"),
+("Yellow           " , "33"),
+("Blue             " , "34"),
+("Magenta          " , "35"),
+("Cyan             " , "36"),
+("White            " , "37"),
+("Bright Black     " , "90"),
+("Bright Red       " , "91"),
+("Bright Green     " , "92"),
+("Bright Yellow    " , "93"),
+("Bright Blue      " , "94"),
+("Bright Magenta   " , "95"),
+("Bright Cyan      " , "96"),
+("Bright White     " , "97"),
+("BG_Black         " , "40"),
+("BG_Red           " , "41"),
+("BG_Green         " , "42"),
+("BG_Yellow        " , "43"),
+("BG_Blue          " , "44"),
+("BG_Magenta       " , "45"),
+("BG_Cyan          " , "46"),
+("BG_White         " , "47"),
+("BG_Bright Black  " , "100"),
+("BG_Bright Red    " , "101"),
+("BG_Bright Green  " , "102"),
+("BG_Bright Yellow " , "103"),
+("BG_Bright Blue   " , "104"),
+("BG_Bright Magenta" , "105"),
+("BG_Bright Cyan   " , "106"),
+("BG_Bright White  " , "107"),
+]}
+colorPrefix = '\033['
+colorSuffix = 'm'
+colorReset = colorPrefix + '0' + colorSuffix
+
+doc = r"""
+Usage: ./colorize.py [-h] [-p <pattern>]... [<input> ...]
+
+    -h,--help                  show this
+    -p,--pattern <pattern>     specify the pattern in "pattern=color" form.
+"""
+def main():
+    # Handle broken pipes when piping the output of this process to other
+    # processes.
+    signal(SIGPIPE,SIG_DFL)
+    options = docopt(doc)
+    patternMap = dict(x.split('=') for x in options['--pattern'])
+
+    def addColor(line, colorNames):
+      colorSequence = None
+      if '|' not in colorNames:
+        colorSequence = colorPrefix + colorMap[colorNames] + colorSuffix
+      else:
+        colorSequence = colorPrefix + ';'.join(colorMap[x] for x in colorNames.split("|")) + colorSuffix
+      return "%s%s%s" % (colorSequence, line, colorReset)
+
+    def colorize(f):
+      for line in f:
+        line = line.strip()
+        for pattern in patternMap:
+          if pattern in line:
+            line = addColor(line, patternMap[pattern])
+        print line
+
+    if len(options["<input>"]) == 0:
+      colorize(stdin)
+    else:
+      for name in options["<input>"]:
+        with open(name) as f:
+          colorize(f)
+
+
+if __name__ == '__main__':
+    main()
+
